14a62db80f13504628912c3f34cba4db82c3f222
lucas-braga-ribeiro <olalucasbraga@gmail.com>
2025-11-11 10:30:26 -0300

    Refactor ArticleCard and ArticleTag components for improved styling and functionality. Update ArticleCard to enhance button interactions and modify ArticleTag to use a more structured color mapping for tags. Adjust SearchBar placeholder to include tags in the search criteria.

--- components/articles/ArticleCard.tsx
+++ components/articles/ArticleCard.tsx
@@ -14,9 +14,9 @@ interface ArticleCardProps {
 
 export function ArticleCard({ article }: ArticleCardProps) {
   return (
-    <div className="flex items-start gap-4 p-4 border-b border-gray-100 dark:border-gray-900 hover:bg-gray-50 dark:hover:bg-gray-950 transition-colors  h-32">
+    <div className="flex items-start gap-4 p-4 border-b border-gray-100 dark:border-gray-900  h-32  hover:bg-gray-50 dark:hover:bg-gray-950 transition-colors">
       <ArticleThumbnail src={article.thumbnail} alt={article.title} />
-      <div className="flex-1 min-w-0">
+      <div className="flex-1 min-w-0 ">
         <h3 className="font-bold text-base text-gray-900 dark:text-gray-100 mb-1 leading-tight">
           {article.title} <a href={article.url} target="_blank" className="text-gray-500 dark:text-gray-400 text-xs">({article.url})</a>
         </h3>
@@ -31,7 +31,8 @@ export function ArticleCard({ article }: ArticleCardProps) {
     
   
   
-        <Button variant="outline" size="sm" className="h-full w-20 rounded-sm flex items-center justify-center bg-gray-100 dark:bg-gray-900 cursor-pointer"> <ThumbsUp className="size-5" /></Button>
+        <Button variant="outline" size="sm" className="group h-full w-20 rounded-sm flex items-center justify-center bg-gray-100 dark:bg-gray-900 cursor-pointer hover:border-gray-300 dark:hover:border-gray-700
+         hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors"> <ThumbsUp className="size-5 group-hover:-rotate-12 group-hover:scale-110 transition-all duration-300" /></Button>
         </div>
      
   );
--- components/articles/ArticleTag.tsx
+++ components/articles/ArticleTag.tsx
@@ -1,15 +1,15 @@
 const ArticleTag = ({ tag }: { tag: string }) => {
-    const tagColors = {
-        tecnologia: "red",
-        startup: "blue",
-        AI: "green",
-        negocios: "purple",
-        economia: "green",
-        ciencia: "indigo",
-        financas: "orange"
+    const tagColors: Record<string, string> = {
+        tecnologia: "bg-sky-100 dark:bg-sky-900 text-sky-500 dark:text-sky-400",
+        startup: "bg-green-100 dark:bg-green-900 text-green-500 dark:text-green-400",
+        AI: "bg-yellow-100 dark:bg-yellow-900 text-yellow-500 dark:text-yellow-400",
+        negocios: "bg-purple-100 dark:bg-purple-900 text-purple-500 dark:text-purple-400",
+        economia: "bg-teal-100 dark:bg-teal-900 text-teal-500 dark:text-teal-400",
+        ciencia: "bg-indigo-100 dark:bg-indigo-900 text-indigo-500 dark:text-indigo-400",
+        financas: "bg-pink-100 dark:bg-pink-900 text-pink-500 dark:text-pink-400"
     }
   return (
-    <span className={`bg-${tagColors[tag as keyof typeof tagColors]}-100 dark:bg-${tagColors[tag as keyof typeof tagColors]}-900 rounded-sm px-2 py-1 text-${tagColors[tag as keyof typeof tagColors]}-500 dark:text-${tagColors[tag as keyof typeof tagColors]}-400 text-xs `}>#{tag}</span>
+    <span className={`${tagColors[tag]} rounded-sm px-2 py-1 text-xs `}>#{tag}</span>
   );
 };
 
